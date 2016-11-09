dce18c11cbdc2cf2f65a8a9b6e375b9fc96bec25
keeneyetact <elaineelainefletcher@gmail.com>
2016-11-09 19:25:50 +0800

    update cron

--- newsSpider/cron.sh
+++ newsSpider/cron.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/
 
-cd /wwwroot/scrapy_news/newsSpider
+#cd /wwwroot/scrapy_news/newsSpider
+cd ~/javin/python/yuqing/newsSpider/
 scrapy crawl beiqingwang 2>&1
 scrapy crawl btime 2>&1
-scrapy crawl jingjiaoribao 2>&1
+scrapy crawl cnr 2>&1
+scrapy crawl focus 2>&1
+scrapy crawl net163 2>&1
+scrapy crawl peoplebj 2>&1
+scrapy crawl people 2>&1
 cd -
