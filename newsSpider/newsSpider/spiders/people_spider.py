7071829a153784cf1d21b299ac0f7bbbc550fc47
keeneyetact <elaineelainefletcher@gmail.com>
2016-11-09 18:31:58 +0800

    add spider peoplebj 央广网-财经

--- newsSpider/newsSpider/spiders/people_spider.py
+++ newsSpider/newsSpider/spiders/people_spider.py
@@ -8,31 +8,30 @@ import string
 from scrapy.http import Request
 from newsSpider.items import NewsItem, NewsContentItem
 
-class PeoplebjSpider(scrapy.Spider):
-    name = "peoplebj"
+class CnrSpider(scrapy.Spider):
+    name = "cnr"
     start_urls = [
-        "http://bj.people.com.cn/GB/82839/index.html"
+        "http://finance.cnr.cn/2014jingji/djbd/"
     ]
-    url_root = "http://bj.people.com.cn/"
 
     def parse(self, response):
         # 获取所有的文章列表
-        article_list = response.xpath('//ul[@class="list_16 mt10"]/li')
+        article_list = response.xpath('//ul[@class="f14 lh24 f12_5a5a5a left"]/li')
         for article in article_list:
             item = NewsItem()
-            item['url'] = self.url_root + article.xpath('a/@href').extract()[0]
+            item['url'] = article.xpath('span/a/@href').extract()[0]
             yield Request(url = item['url'], callback = self.parseContent)
 
     def parseContent(self, response):
         # 获取文章各部分信息
-        article = response.selector.xpath('//body')
+        article = response.selector.xpath('//div[@class="wh645 left"]')
         item = NewsContentItem()
-        item['title'] = article.xpath('//h1/text()').extract()[0]
-        contents = article.xpath('//div[@class="box_con"]/p/text()').extract()
+        item['title'] = article.xpath('p/text()').extract()[0]
+        contents = article.xpath('//div[@class="TRS_Editor"]/p/text()').extract()
         item['content'] = ''
         for cont in contents:
             item['content'] += cont.strip() + '<br />'
         item['url']   = response.url
         item['time']  = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
-        item['site']  = '人民网-北京频道-财经'
+        item['site']  = '央广网-财经'
         yield item
